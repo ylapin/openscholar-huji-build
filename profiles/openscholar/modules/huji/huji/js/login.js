f20197f04640e9dce3b1d57ad1fa9991bb497221
Roy Segall <roy.segall@gizra.com>
2014-09-22 12:18:11 +0300

    Adding SAML integration.

---
+++ profiles/openscholar/modules/huji/huji/js/login.js
@@ -0,0 +1,10 @@
+function toggle() {
+
+  var ele = document.getElementById("localogin");
+
+  if(ele.style.display == "block") {
+    ele.style.display = "none";
+  } else {
+    ele.style.display = "block";
+  }
+}
